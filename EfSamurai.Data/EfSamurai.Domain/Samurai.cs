70693dc4dc8361a255c5b932951b2bc224a4eec3
Heidivonjahf <heidivonjahf@hotmail.com>
2019-01-17 14:13:26 +0100

    Migrerat

--- EfSamurai.Data/EfSamurai.Domain/Samurai.cs
+++ EfSamurai.Data/EfSamurai.Domain/Samurai.cs
@@ -10,7 +10,7 @@ namespace EfSamurai.Domain
         public int Id { get; set; }
         public string Name { get; set; }
         public int Age { get; set; }
-
+        public List<Quote> ListOfQuotes { get; set; }
 
     }
 }
