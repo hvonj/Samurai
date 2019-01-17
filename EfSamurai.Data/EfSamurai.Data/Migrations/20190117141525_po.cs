12033bbc5477b772eeae5ec3e1f000ed9105a5ea
Heidivonjahf <heidivonjahf@hotmail.com>
2019-01-17 15:16:11 +0100

    Korr. ett fel i BattleLog

---
+++ EfSamurai.Data/EfSamurai.Data/Migrations/20190117141525_po.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EfSamurai.Data.Migrations
+{
+    public partial class po : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BattleEvents_Battle_BattleId",
+                table: "BattleEvents");
+
+            migrationBuilder.DropIndex(
+                name: "IX_BattleEvents_BattleId",
+                table: "BattleEvents");
+
+            migrationBuilder.DropColumn(
+                name: "BattleId",
+                table: "BattleEvents");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "BattleId",
+                table: "BattleEvents",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BattleEvents_BattleId",
+                table: "BattleEvents",
+                column: "BattleId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BattleEvents_Battle_BattleId",
+                table: "BattleEvents",
+                column: "BattleId",
+                principalTable: "Battle",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
